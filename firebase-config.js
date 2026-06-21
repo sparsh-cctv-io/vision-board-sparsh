7939dc298f97d450285eb6907c891146bfc2a8ba
ojassyadav19 <ojassyadav19@users.noreply.github.com>
2026-06-21 10:26:18 +0000

    deploy: bb3fad17fe9acc17d0667eeeb810f2c26fb944ca

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 var FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyArqcRN8I2q_nssxOhlkl41p796n87mSlg",
+  authDomain: "sparsh-vision-board.firebaseapp.com",
+  databaseURL: "https://sparsh-vision-board-default-rtdb.firebaseio.com",
+  projectId: "sparsh-vision-board",
+  storageBucket: "sparsh-vision-board.firebasestorage.app",
+  messagingSenderId: "57394026210",
+  appId: "1:57394026210:web:b224a2578e2a9986b2f0bb"
 };
